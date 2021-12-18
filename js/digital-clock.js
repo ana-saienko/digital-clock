c57e8a8023b411d8fda5ba755fbcaa6a757211ad
ana-saienko <saienko_ana@icloud.com>
2021-12-18 15:58:15 +0100

    time and text, and bg

--- js/digital-clock.js
+++ js/digital-clock.js
@@ -24,3 +24,32 @@ function setTime(){
     document.getElementById('m1').innerHTML = m.charAt(1);
   }
 }
+
+// --------------------------------------------------------------
+
+var now = new Date();
+var hours = now.getHours();
+var ft = now.toLocaleString("en-US", {
+	 hour: "numeric",
+	 minute: "numeric",
+	 hour12: true
+});
+if (6 <= hours && hours < 10) {//Morning
+	 document.write('<body style="background-image: url(../images/bg-day.jpg)">');
+	 var type = "Good Morning";
+}
+if (10 <= hours && hours < 16) {//Day
+	 document.write('<body style="background-image: url(../images/bg-day.jpg)">');
+	 var type = "Good day";
+}
+if (16 <= hours && hours < 22) {//Evening
+	 document.write('<body style="background-image: url(../images/bg-night.jpg)">');
+	 var type = "Good Evening";
+}
+if (22 <= hours && hours < 6) {//Night
+	 document.write('<body style="background-image: url(../images/bg-night.jpg)">');
+	 var type = "Good night";
+}
+document.getElementById("main-clock__title").innerHTML = `${type}, IT’S CURRENTLY`;
+//CSS gradient backgrounds from https://uigradients.com
+// https://write.corbpie.com/change-background-and-text-based-on-time-with-javascript/
