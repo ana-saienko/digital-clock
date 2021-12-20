69b56fef8aaf05c7f40988f5747ae51054417a02
saienko-ana <saienko_ana@icloud.com>
2021-12-20 13:17:37 +0100

    done js part of the modal TIME

--- js/modal-details.js
+++ js/modal-details.js
@@ -14,3 +14,28 @@ if (btnMore) {
 
 const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 document.getElementById("current-timezone").innerHTML = `${timezone}`;
+
+// Day of the year
+
+var now = new Date();
+var start = new Date(now.getFullYear(), 0, 0);
+var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
+var oneDay = 1000 * 60 * 60 * 24;
+var day = Math.floor(diff / oneDay);
+
+document.getElementById("day-week").innerHTML = `${day}`;
+
+let dayWeek = now.getDay();
+document.getElementById("day-one-week").innerHTML = `${dayWeek}`;
+
+// Week num
+
+function getNumberOfWeek() {
+	const today = new Date();
+	const firstDayOfYear = new Date(today.getFullYear(), 0, 1);
+	const pastDaysOfYear = (today - firstDayOfYear) / 86400000;
+	return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
+}
+let weekYear = getNumberOfWeek();
+
+document.getElementById("week-num").innerHTML = `${weekYear}`;
